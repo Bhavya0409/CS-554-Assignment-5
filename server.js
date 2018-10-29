e5c35202fef3e008ee35a032e16bfb7b7e96369d
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-10-29 11:43:28 -0400

    used promised version of cache get users

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Promise = require('bluebird');
-const cache = require('express-redis-cache')();
+const cache = Promise.promisifyAll(require('express-redis-cache')());
 const getById = require('./dataModule.js');
 
 const app = express();
@@ -15,48 +15,47 @@ app.get('/api/people/:id', (req, res) => {
   let {id} = req.params;
   id = parseInt(id);
 
-  cache.get('users', async (err, cacheRes) => {
-    if (err) {
-      res.send('something broke')
-    } else {
-      const cachedUsers = getCachedUsers(cacheRes);
-      if (cachedUsers === undefined) {
-        // no users in cache, so make a new entry
-        const user = await getById(id);
+  cache.getAsync('users').then(async (cacheRes) => {
+    const cachedUsers = getCachedUsers(cacheRes);
+    if (cachedUsers === undefined) {
+      // no users in cache, so make a new entry
+      const user = await getById(id);
 
-        cache.add('users', JSON.stringify({users: [user]}), (err, add) => {
+      cache.add('users', JSON.stringify({users: [user]}), (err, add) => {
+        if (err) {
+          throw err;
+        } else {
+          // added user to cache, so send user back
+          res.send(user);
+        }
+      });
+
+    } else {
+      // cached users exist
+      const user = cachedUsers.find((user) => {
+        return user.id === id;
+      });
+
+      if (user === undefined) {
+        // couldn't find user in cache so look for user in mockData
+        const user = await getById(id);
+        cachedUsers.push(user);
+        cache.add('users', JSON.stringify({users: cachedUsers}), (err, add) => {
           if (err) {
             throw err;
           } else {
-            // added user to cache, so send user back
             res.send(user);
           }
         });
-
       } else {
-        // cached users exist
-        const user = cachedUsers.find((user) => {
-          return user.id === id;
-        });
-
-        if (user === undefined) {
-          // couldn't find user in cache so look for user in mockData
-          const user = await getById(id);
-          cachedUsers.push(user);
-          cache.add('users', JSON.stringify({users: cachedUsers}), (err, add) => {
-            if (err) {
-              throw err;
-            } else {
-              res.send(user);
-            }
-          });
-        } else {
-          // found user - >return user
-          res.send(user);
-        }
+        // found user - >return user
+        res.send(user);
       }
     }
+  }).catch(err => {
+    throw err;
   })
+
 });
 
 /**
