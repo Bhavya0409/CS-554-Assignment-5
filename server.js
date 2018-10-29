250e02f48a69188853da7e53cb491d3b0deea8e3
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-10-29 12:08:32 -0400

    use async await instead of promise chaining

--- server.js
+++ server.js
@@ -10,51 +10,34 @@ app.get('/api/people/history', (req, res) => {
   res.send('history route');
 });
 
-app.get('/api/people/:id', (req, res) => {
+app.get('/api/people/:id', async (req, res) => {
 
-  let {id} = req.params;
-  id = parseInt(id);
+  const id = parseInt(req.params.id);
+  const cacheRes = await cache.getAsync('users');
 
-  cache.getAsync('users').then(async (cacheRes) => {
-    const cachedUsers = getCachedUsers(cacheRes);
-    if (cachedUsers === undefined) {
-      // no users in cache, so make a new entry
+  const cachedUsers = getCachedUsers(cacheRes);
+  if (cachedUsers === undefined) {
+    // no users in cache, so make a new entry
+    const user = await getById(id);
+    await cache.addAsync('users', JSON.stringify({users: [user]}));
+    res.send(user);
+  } else {
+    // cached users exist
+    const user = cachedUsers.find((user) => {
+      return user.id === id;
+    });
+
+    if (user === undefined) {
+      // couldn't find user in cache so look for user in mockData
       const user = await getById(id);
-
-      cache.add('users', JSON.stringify({users: [user]}), (err, add) => {
-        if (err) {
-          throw err;
-        } else {
-          // added user to cache, so send user back
-          res.send(user);
-        }
-      });
-
+      cachedUsers.push(user);
+      await cache.addAsync('users', JSON.stringify({users: cachedUsers}));
+      res.send(user);
     } else {
-      // cached users exist
-      const user = cachedUsers.find((user) => {
-        return user.id === id;
-      });
-
-      if (user === undefined) {
-        // couldn't find user in cache so look for user in mockData
-        const user = await getById(id);
-        cachedUsers.push(user);
-        cache.add('users', JSON.stringify({users: cachedUsers}), (err, add) => {
-          if (err) {
-            throw err;
-          } else {
-            res.send(user);
-          }
-        });
-      } else {
-        // found user - >return user
-        res.send(user);
-      }
+      // found user - >return user
+      res.send(user);
     }
-  }).catch(err => {
-    throw err;
-  })
+  }
 
 });
 
